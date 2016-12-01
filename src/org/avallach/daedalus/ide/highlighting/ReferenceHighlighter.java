16e6509aae0b551a25a1b232e66cf3136f66df6f
Unknown <github@avallach.ovh>
2016-12-01 19:07:58 +0100

    Fixed highlighter of unresolved references

--- src/org/avallach/daedalus/ide/highlighting/ReferenceHighlighter.java
+++ src/org/avallach/daedalus/ide/highlighting/ReferenceHighlighter.java
@@ -2,11 +2,32 @@ package org.avallach.daedalus.ide.highlighting;
 
 import com.intellij.codeHighlighting.TextEditorHighlightingPassRegistrar;
 import com.intellij.codeInsight.daemon.impl.HighlightInfoType;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.openapi.editor.markup.EffectType;
+import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
+import com.intellij.ui.JBColor;
+
+import java.awt.*;
 
 public class ReferenceHighlighter extends PsiHighlighterFactoryBase {
+
+    public static HighlightInfoType unresolvedReferenceHighlight = new HighlightInfoType.HighlightInfoTypeImpl(
+            HighlightSeverity.INFORMATION,
+            TextAttributesKey.createTextAttributesKey(
+                    "UNRESOLVED_REFERENCE",
+                    new TextAttributes
+                            (
+                                    JBColor.RED,
+                                    null,
+                                    JBColor.RED,
+                                    EffectType.WAVE_UNDERSCORE,
+                                    Font.PLAIN
+                            ))
+    );
 
     public ReferenceHighlighter(Project project, TextEditorHighlightingPassRegistrar registrar) {
         super(project, registrar);
@@ -19,6 +40,6 @@ public class ReferenceHighlighter extends PsiHighlighterFactoryBase {
 
     @Override
     protected HighlightInfoType getStyle(PsiElement element) {
-        return isUnresolvedReference(element) ? DefinitionHighlighter.constantHighlight : null;
+        return isUnresolvedReference(element) ? unresolvedReferenceHighlight : null;
     }
 }
